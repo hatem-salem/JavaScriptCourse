1b2ecc5659fff30861d636e771b1ef91e1c5b365
hatem-salem <58885673+hatem-salem@users.noreply.github.com>
2024-03-26 11:21:50 +0000

    Initial skeleton

---
+++ website/index.js
@@ -0,0 +1,13 @@
+// console.log(`Hello`);
+// console.log(`I like Pizza`);
+// window.alert(`This is an alert`);
+// window.alert(`I like Pizza`);
+document.getElementById("myH1").textContent = `Hello`;
+document.getElementById("myP").textContent = `I like Pizza!`;
+//This is a comment
+/*
+  This 
+  is 
+  a 
+  comment
+*/
