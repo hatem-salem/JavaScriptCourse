11039a152472f8c8f4f0b023e206f01c2320b7f6
hatem-salem <58885673+hatem-salem@users.noreply.github.com>
2024-03-26 15:45:50 +0000

    type conversion

--- website/index.js
+++ website/index.js
@@ -1,6 +1,11 @@
-let username;
-document.getElementById("mySubmit").onclick = function () {
-  username = document.getElementById("username").value;
-  document.getElementById("myH1").textContent = `Hello ${username}`;
-  console.log(username);
-};
+let x;
+let y;
+let z;
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
+
