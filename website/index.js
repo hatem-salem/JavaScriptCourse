2fbeb69028bc23ba7bc33f673ac57a8b66ef6726
hatem-salem <58885673+hatem-salem@users.noreply.github.com>
2024-03-28 23:50:09 +0000

    Image Slider

--- website/index.js
+++ website/index.js
@@ -1,43 +1,30 @@
-const choices = ["rock", "paper", "scissors"];
-const playerDisplay = document.getElementById("playerDisplay");
-const computerDisplay = document.getElementById("computerDisplay");
-const resultDisplay = document.getElementById("resultDisplay");
-const playerScoreDisplay = document.getElementById("playerScoreDisplay");
-const computerScoreDisplay = document.getElementById("computerScoreDisplay");
-let playerScore = 0;
-let computerScore = 0;
-function playGame(playerChoice) {
-  const computerChoice = choices[Math.floor(Math.random() * 3)];
-  let result = "";
-  if (computerChoice === playerChoice) {
-    result = "IT'S A TIE";
-  } else {
-    switch (playerChoice) {
-      case "rock":
-        result = (computerChoice === "scissors") ? "YOU WIN!" : "YOU LOSE!";
-        break;
-      case "paper":
-        result = (computerChoice === "rock") ? "YOU WIN!" : "YOU LOSE!";
-        break;
-      case "scissors":
-        result = (computerChoice === "paper") ? "YOU WIN!" : "YOU LOSE!";
-        break;
-    }
-  }
-  playerDisplay.textContent = `PLAYER:${playerChoice}`;
-  computerDisplay.textContent = `COMPUTER:${computerChoice}`;
-  resultDisplay.textContent = result;
-  resultDisplay.classList.remove("greenText", "redText");
-  switch (result) {
-    case "YOU WIN!":
-      resultDisplay.classList.add("greenText");
-      playerScore++;
-      playerScoreDisplay.textContent = playerScore;
-      break;
-    case "YOU LOSE!":
-      resultDisplay.classList.add("redText");
-      computerScore++;
-      computerScoreDisplay.textContent = computerScore;
-      break;
-  }
+const slides = document.querySelectorAll(".slides img");
+let slideIndex = 0;
+let intervalId = null;
+function initializeSlider() {
+  slides[slideIndex].classList.add("displaySlide");
+  intervalId = setInterval(nextSlide, 5000);
 }
+function showSlide(index) {
+  if (index >= slides.length) {
+    slideIndex = 0;
+  } else if (index < 0) {
+    slideIndex = slides.length - 1;
+  }
+  slides.forEach(slide => {
+    slide.classList.remove("displaySlide");
+  });
+  slides[slideIndex].classList.add("displaySlide");
+}
+function prevSlide() {
+  clearInterval(intervalId);
+  slideIndex--;
+  showSlide(slideIndex);
+}
+function nextSlide() {
+
+  slideIndex++;
+  showSlide(slideIndex);
+
+}
+document.addEventListener("DOMContentLoaded", initializeSlider);
