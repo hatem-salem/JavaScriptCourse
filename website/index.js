538ccb9d4d85c2f3e807d946a575e360469c1a0d
hatem-salem <58885673+hatem-salem@users.noreply.github.com>
2024-03-28 11:34:38 +0000

    Stopwatch Program

--- website/index.js
+++ website/index.js
@@ -1,14 +1,42 @@
-function updateClock() {
-  const now = new Date();
-  let hours = now.getHours();
-  const meridiem = hours >= 12 ? "PM" : "AM";
-  hours = hours % 12 || 12;
-  hours = hours.toString().padStart(2, 0);
-  const minutes = now.getMinutes().toString().padStart(2, 0);
-  const seconds = now.getSeconds().toString().padStart(2, 0);
-  const timeString = `${hours}:${minutes}:${seconds} ${meridiem}`;
-  document.getElementById("clock").textContent = timeString;
+const display = document.getElementById("display");
+let timer = null;
+let startTime = 0;
+let elapsedTime = 0;
+let isRunning = false;
+function start() {
+  if (!isRunning) {
+    isRunning = true;
+    startTime = Date.now() - elapsedTime;
+    timer = setInterval(update, 10);
+  }
 }
-setInterval(updateClock, 1000);
+function stop() {
+  if (isRunning) {
+    isRunning = false;
+    clearInterval(timer);
+    elapsedTime = Date.now() - startTime;
+  }
+
+}
+function reset() {
+  isRunning = false;
+  clearInterval(timer);
+  startTime = 0;
+  elapsedTime = 0;
+  display.textContent = "00:00:00:00";
 
 
+}
+function update() {
+  const currentTime = Date.now();
+  elapsedTime = currentTime - startTime;
+  let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+  let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+  let seconds = Math.floor(elapsedTime / 1000 % 60);
+  let milliseconds = Math.floor(elapsedTime % 1000 / 10);
+  hours = String(hours).padStart(2, 0);
+  minutes = String(minutes).padStart(2, 0);
+  seconds = String(seconds).padStart(2, 0);
+  milliseconds = String(milliseconds).padStart(2, 0);
+  display.textContent = `${hours}:${minutes}:${seconds}:${milliseconds}`;
+}
