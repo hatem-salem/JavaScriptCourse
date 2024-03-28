5354ed865bd04c8c8a44885a20cd598418f1d56f
hatem-salem <58885673+hatem-salem@users.noreply.github.com>
2024-03-28 12:47:20 +0000

    Calculator Program

--- website/index.js
+++ website/index.js
@@ -1,2 +1,15 @@
-import { PI, getCircumference, getArea, getSphereArea } from "./mathUtil.js";
-console.log(PI);
+const display = document.getElementById("display");
+function appendToDisplay(input) {
+  display.value += input;
+}
+function clearDisplay() {
+  display.value = "";
+}
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "Error";
+  }
+
+}
