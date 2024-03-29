ca858e783b0392bc257241032b63f20dae9f1d3c
hatem-salem <58885673+hatem-salem@users.noreply.github.com>
2024-03-29 18:25:46 +0000

    Promises

--- website/index.js
+++ website/index.js
@@ -1,26 +1,49 @@
-function walkDog(callback) {
-  setTimeout(() => {
-    console.log("You walk the dog 🐕");
-    callback();
-  }, 1500);
-}
-function cleanKitchen(callback) {
-  setTimeout(() => {
-    console.log("You clean the kitchen 🧹");
-    callback();
-  }, 2500);
-}
-function takeoutTrash(callback) {
-  setTimeout(() => {
-    console.log("You take out the trash 🚮");
-    callback;
-  }, 500);
-}
+function walkDog() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const dogWalked = false;
+      if (dogWalked) {
+        resolve("You walk the dog 🐕");
+      } else {
+        reject("You DIDN't Walk the dog");
+      }
 
-walkDog(() => {
-  cleanKitchen(() => {
-    takeoutTrash(() => {
-      console.log("You finished all the chores!");
-    });
+    }, 1500);
+
   });
-});
+
+}
+function cleanKitchen() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const kitchenCleaned = false;
+      if (kitchenCleaned) {
+        resolve("You clean the kitchen 🧹");
+      } else {
+        reject("You DIDN'T clean the kitchen");
+      }
+    }, 2500);
+
+  });
+}
+function takeoutTrash() {
+  return new Promise((resolve, reject) => {
+
+    setTimeout(() => {
+      const trashTakenout = true;
+      if (trashTakenout) {
+        resolve("You take out the trash 🚮");
+      } else {
+        reject("You DIDN'T take out the trash");
+      }
+    }, 500);
+
+  });
+}
+walkDog()
+  .then(value => { console.log(value); return cleanKitchen(); })
+  .then(value => { console.log(value); return takeoutTrash(); })
+  .then(value => { console.log(value); console.log("You finished all chores!"); })
+  .catch(error => console.error(error));
+console.log("Hello World");
+console.log(walkDog());
